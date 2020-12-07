ccf0c92c92a0a758967b052fe0401345101240ad
Shubh Pathak <shubhpathak07@gmail.com>
2020-12-08 01:01:00 +0530

    Added Room Model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import string,random
 
-# Create your models here.
+
+def generate_unique_code():
+    length=6
+    while(1):
+        code = ''.join(random.choices(string.ascii_uppercase,k=length))
+        if(Room.objects.filter(code=code).count())==0:
+            break
+    return code
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8,default="",unique=True)
+    host = models.CharField(max_length=50,unique=True)
+    guest_can_pause = models.BooleanField(default=False)
+    votes_to_skip = models.IntegerField(default=1)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Room Code:{self.code}, Created by:{self.host}"
