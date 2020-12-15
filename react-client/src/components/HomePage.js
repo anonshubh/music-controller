3335244cbf4644314e8687f3038515c034d10df6
Shubh Pathak <shubhpathak07@gmail.com>
2020-12-15 13:57:20 +0530

    Added renderHomePage Method

--- react-client/src/components/HomePage.js
+++ react-client/src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
+import {Grid,Button,ButtonGroup,Typography} from '@material-ui/core';
+
 import CreateRoomPage from './CreateRoomPage'
 import RoomJoinPage from './RoomJoinPage'
 import Room from './Room'
@@ -10,11 +12,30 @@ class HomePage extends Component {
         super(props);
         this.state = {}
     }
+
+    renderHomePage(){
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align='center'>
+                    <Typography variant="h3" component="h3">Rock n Roll</Typography>
+                </Grid>
+                <Grid item xs={12} align='center'>
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color='primary' to='/join' component={Link}>Join a Room</Button>
+                        <Button color='secondary' to='/create' component={Link}>Create a Room</Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route exact path='/'><p>The Home Page</p></Route>
+                    <Route exact path='/'>
+                        {this.renderHomePage()}
+                    </Route>
                     <Route path='/join' component={RoomJoinPage} />
                     <Route path='/create' component={CreateRoomPage} />
                     <Route path='/room/:roomCode' component={Room} />
